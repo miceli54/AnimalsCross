583d9ba9c90f661259fd65fae406f6c8e978ab78
Nick Miceli <nick.miceli54@gmail.com>
2020-04-07 20:20:18 -0400

    does this work

--- example/views.py
+++ example/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Oi, is this fish really on land?")
+    return HttpResponse("Oi, is that fish really on land?")
