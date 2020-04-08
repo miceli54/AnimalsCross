b0d5c4557644d33b24eab4760668d431646dee65
Nick Miceli <nick.miceli54@gmail.com>
2020-04-07 23:34:17 -0400

    ADDED SECRET BACK BC RETARD

--- example/views.py
+++ example/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1>Oi, is that fish really on land?</h1>")
 
+
+def secret(request):
+    return HttpResponse("<h1>Oi!</h1>")
