305622dbe776e429b4ae996d5887131d94c3edf8
Nick Miceli <nick.miceli54@gmail.com>
2020-04-07 23:26:06 -0400

    added secret page

--- example/views.py
+++ example/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Oi, is that fish really on land?</h1>")
+
+
+def secret(request):
+    return HttpResponse("<h1>Oi!</h1>")
