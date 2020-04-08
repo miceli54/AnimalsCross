aa5ed1151d1a51879a9964df07baeb33f0033ab7
Nick Miceli <nick.miceli54@gmail.com>
2020-04-07 23:19:55 -0400

    Now we made it better

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,6 @@ from example import views
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^admin/', include(admin.site.urls)),
+    url('', views.home, name='homepage'),
+    url('admin/', include(admin.site.urls)),
 ]
